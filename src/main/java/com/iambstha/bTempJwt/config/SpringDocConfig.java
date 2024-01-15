3799f869b2e38edd004076a1b80d86d6dbe0d2ef
iambstha <iambstha@gmail.com>
2024-01-15 11:30:08 +0545

    issue with swagger security scheme solved

--- src/main/java/com/iambstha/bTempJwt/config/SpringDocConfig.java
+++ src/main/java/com/iambstha/bTempJwt/config/SpringDocConfig.java
@@ -4,7 +4,11 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 @Configuration
+@SecurityScheme(name = "Bearer Authentication", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class SpringDocConfig {
 	
     @Bean
